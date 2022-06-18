d71dac9454b52fdc7d719385857e9ba002b346a7
smori <shinichiro.mori.1983@gmail.com>
2022-06-19 01:34:38 +0900

    Initial implement of additionalPages factory

---
+++ src/factory.js
@@ -0,0 +1,118 @@
+/**
+ * @typedef {import('vuepress-types').Page} Page
+ * @typedef {import('vuepress-types').PageOptions} PageOptions
+ */
+
+/**
+ * @param {Page[]} pages
+ * @param {Object} config
+ * @return {Partial<PageOptions>[]}
+ */
+const build = (pages, config) => {
+  /**
+   * @type {Page[]}
+   */
+  const targetPages = [];
+
+  pages.forEach((page) => {
+    if (isTargetPage(page, config)) {
+      targetPages.push(page);
+    }
+  });
+
+  const additionalPages = [];
+
+  const indexLines = [];
+  indexLines.push(`# ${config.indexPage.title}`);
+  indexLines.push('');
+  targetPages.forEach((page) => {
+    indexLines.push(`- [${page.title}](${page.regularPath})`);
+  });
+
+  config.listPages.forEach((listPageConfig) => {
+    const dimension = listPageConfig.dimension;
+    const dimensionValues = getDimensionValues(targetPages, config, dimension);
+
+    indexLines.push(`## ${listPageConfig.indexTitle}`);
+    indexLines.push('');
+
+    dimensionValues.forEach((dimensionValue) => {
+      const lines = [];
+      const pageTitle = listPageConfig.title.replace(':dimension', dimensionValue);
+
+      lines.push(`# ${pageTitle}`);
+      lines.push('');
+      targetPages.forEach((page) => {
+        if (page.frontmatter[config.key][dimension] === dimensionValue) {
+          lines.push(`- [${page.title}](${page.regularPath})`);
+        }
+      });
+
+      const path = listPageConfig.path.replace(':dimension', dimensionValue);
+
+      additionalPages.push({
+        path: path,
+        content: lines.join('\n'),
+      });
+
+      indexLines.push(`- [${pageTitle}](${path})`);
+    });
+  });
+
+  additionalPages.push({
+    path: config.indexPage.path,
+    frontmatter: {
+      title: config.indexPage.title,
+    },
+    content: indexLines.join('\n'),
+  });
+
+  return additionalPages;
+};
+
+/**
+ * @param {Page} page
+ * @param {Object} config
+ * @return {boolean}
+ */
+const isTargetPage = (page, config) => {
+  if (!page.frontmatter[config.key]) {
+    return false;
+  }
+
+  const pageData = page.frontmatter[config.key];
+
+  for (let i = 0; i < config.dimensions.length; i++) {
+    const dimension = config.dimensions[i];
+
+    if (!pageData.hasOwnProperty(dimension.name)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+/**
+ * @param {Page[]} pages
+ * @param {Object} config
+ * @param {string} dimension
+ */
+const getDimensionValues = (pages, config, dimension) => {
+  /**
+   * @type {Set<any>}
+   */
+  const values = new Set();
+
+  pages.forEach((page) => {
+    values.add(page.frontmatter[config.key][dimension]);
+  });
+
+  const result = Array.from(values);
+
+  result.sort();
+
+  return result;
+};
+
+module.exports.build = build;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@
  * @typedef {import('vuepress-types').PluginOptionAPI} PluginOptionAPI
  */
 
+const factory = require('./factory');
+
 /**
  * @param {Object} options
+ * @param {Context} context
  * @return {PluginOptionAPI}
  */
-module.exports = (options) => {
+module.exports = (options, context) => {
+  const prepareAdditionalPages = async () => {
+    return factory.build(context.pages, options.config || {});
+  };
+
   return {
     name: 'vuepress-plugin-frontmatter-multisort',
+    async additionalPages() {
+      return await prepareAdditionalPages();
+    },
   };
 };
